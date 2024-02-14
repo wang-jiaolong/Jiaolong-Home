ab6ed102f3d4d714bdad8486ebd00c0e7ff28ef5
Jiaolong <jiaolong.dev@outlook.com>
2024-02-14 17:24:23 +0800

    refactor

--- docs/.vitepress/rewrites.js
+++
@@ -1,5 +0,0 @@
-export const rewrites = {
-  "pages/theme/00-vitepress.md": "pages/theme/vitepress.md",
-  "pages/theme/01-theme.md": "pages/theme/theme.md",
-  "pages/theme/02-example.md": "pages/theme/example.md"
-}
