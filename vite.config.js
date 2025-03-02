3bc67de63605e208d25543bd86462b95f8ce6658
Jiaolong <wangjiaolong@bilibili.com>
2025-03-02 19:14:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-import imagemin from "vite-plugin-imagemin";
-
 export default {
-  plugins: [
-    imagemin({
-      mozjpeg: { quality: 80 },
-      pngquant: { quality: [0.7, 0.9] },
-      webp: { quality: 80 },
-    }),
-  ],
-};
+    assetsInclude: ["**/*.heic"]
+  }
